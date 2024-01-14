b06d1afbc2f8d8429d61902cdebcd2ada18d928a
iliya-b <barutkinila@gmail.com>
2024-01-14 17:38:37 +0300

    Generalize Tane and PFDTane

--- src/core/algorithms/fd/pfdtane/pfdtane.h
+++ src/core/algorithms/fd/pfdtane/pfdtane.h
@@ -2,7 +2,7 @@
 
 #include <string>
 
-#include "algorithms/fd/tane/tane.h"
+#include "algorithms/fd/tane/common_tane.h"
 #include "config/error_measure/type.h"
 #include "enums.h"
 #include "model/table/position_list_index.h"
@@ -10,38 +10,21 @@
 
 namespace algos {
 
-class PFDTane : public PliBasedFDAlgorithm {
+class PFDTane : public tane::CommonTane {
 private:
-    void ResetStateFd() final;
     void RegisterOptions();
     void MakeExecuteOptsAvailable() final;
-    unsigned long long ExecuteInternal() override final;
 
 public:
-    config::ErrorType max_fd_error_;
-    config::ErrorType max_ucc_error_;
-    config::MaxLhsType max_lhs_;
-    ErrorMeasure error_measure_ = ErrorMeasure::_values()[0];
-
-    int count_of_fd_ = 0;
-    int count_of_ucc_ = 0;
-    long apriori_millis_ = 0;
+    ErrorMeasure error_measure_ = +ErrorMeasure::per_value;
 
     PFDTane();
 
-    model::PositionListIndex *GetColumnIndex(unsigned int column); // for test
+    model::PositionListIndex* GetColumnIndex(unsigned int column);  // for test
 
-    static double CalculateUccError(model::PositionListIndex const* pli,
-                                    ColumnLayoutRelationData const* relation_data);
-
-    // static double round(double error) { return ((int)(error * 32768) + 1)/ 32768.0; }
-
-    void RegisterAndCountFd(Vertical const& lhs, Column const* rhs, double error,
-                            RelationalSchema const* schema);
-    // void RegisterFd(Vertical const* lhs, Column const* rhs, double error, RelationalSchema const*
-    // schema);
-    void RegisterUcc(Vertical const& key, double error, RelationalSchema const* schema);
-
+    double CalculateZeroAryFdError(ColumnData const* rhs) override;
+    double CalculateFdError(model::PositionListIndex const* x_pli,
+                            model::PositionListIndex const* xa_pli) override;
     double CalculateZeroAryFdErrorPerValue(ColumnData const* rhs);
     double CalculateFdErrorPerValue(model::PositionListIndex const* x_pli,
                                     model::PositionListIndex const* xa_pli);
