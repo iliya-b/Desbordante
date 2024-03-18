89518f217f8f897fa14facd3503f776d147e475a
Ilia <barutkinila@gmail.com>
2024-03-18 19:40:40 +0300

    Refactor TANE-based algorithms
    Generalize TANE and PFDTANE algorithms,
    remove AUCC discovery artifacts from them,
    perform additional pruning, add tests.

--- src/core/algorithms/fd/pfdtane/pfdtane.h
+++ src/core/algorithms/fd/pfdtane/pfdtane.h
@@ -2,6 +2,7 @@
 
 #include "algorithms/fd/pli_based_fd_algorithm.h"
 #include "algorithms/fd/tane/lattice_level.h"
+#include "algorithms/fd/tane/tane_common.h"
 #include "config/error/type.h"
 #include "config/error_measure/type.h"
 #include "config/max_lhs/type.h"
@@ -11,31 +12,23 @@
 
 namespace algos {
 
-class PFDTane : public PliBasedFDAlgorithm {
+class PFDTane : public TaneCommon {
 private:
-    config::ErrorType max_fd_error_;
-    config::ErrorType max_ucc_error_;
-    config::MaxLhsType max_lhs_;
     ErrorMeasure error_measure_ = +ErrorMeasure::per_tuple;
-
-    void ResetStateFd() final;
     void RegisterOptions();
     void MakeExecuteOptsAvailable() final;
-    void Prune(model::LatticeLevel* level);
-    void ComputeDependencies(model::LatticeLevel* level);
-    unsigned long long ExecuteInternal() final;
+    config::ErrorType CalculateZeroAryFdError(ColumnData const* rhs) override;
+    config::ErrorType CalculateFdError(model::PositionListIndex const* lhs_pli,
+                                       model::PositionListIndex const* joint_pli) override;
 
 public:
     PFDTane();
-    static double CalculateUccError(model::PositionListIndex const* pli,
-                                    ColumnLayoutRelationData const* relation_data);
-
-    void RegisterAndCountFd(Vertical const& lhs, Column const* rhs, double error,
-                            RelationalSchema const* schema);
-    static config::ErrorType CalculateZeroAryFdError(ColumnData const* rhs);
-    static config::ErrorType CalculateFdError(model::PositionListIndex const* x_pli,
+    static config::ErrorType CalculateZeroAryPFDError(ColumnData const* rhs,
+                                                     ColumnLayoutRelationData const* relation_data);
+    static config::ErrorType CalculatePFDError(model::PositionListIndex const* x_pli,
                                               model::PositionListIndex const* xa_pli,
-                                              ErrorMeasure error_measure);
+                                              ErrorMeasure error_measure,
+                                              ColumnLayoutRelationData const* relation_data);
 };
 
 }  // namespace algos
